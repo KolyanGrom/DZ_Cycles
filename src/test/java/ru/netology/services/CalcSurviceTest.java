f2bb9e275055410a95ece307b5cf4200164b485b
KolyanGrom <kolyangrom@yandex.ru>
2024-02-10 09:58:21 +0700

    Подключение параметризированного теста с данными из файла

--- src/test/java/ru/netology/services/CalcSurviceTest.java
+++ src/test/java/ru/netology/services/CalcSurviceTest.java
@@ -3,11 +3,10 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcSurviceTest {
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/parameters.csv" )
+    @CsvFileSource(files = "src/test/resources/parameters.csv")
     public void shouldCalculate(int income, int expenses, int threshold, int expected) {
         CalcSurvice survice = new CalcSurvice();
 
